c08cb87016d933c63044216a58fc6ec27b25f0a9
Mu Yang <emfomy@gmail.com>
2020-02-13 15:39:05 +0800

    Add WsSentenceList. Implement to_text() for WS utilities.

--- ckipnlp/util/ws.py
+++ ckipnlp/util/ws.py
@@ -19,36 +19,90 @@ class WsWord(NamedTuple):
     pos: str  #: *str* – the post-tag.
 
     @classmethod
-    def from_text(cls, text: str):
-        """Create :class:`WsWord` object from :class:`ckipnlp.ws.CkipWs` output."""
+    def from_text(cls, text):
+        """Create a :class:`WsWord` object from :class:`ckipnlp.ws.CkipWs` output.
+
+        Parameters
+        ----------
+            text : str
+                A word from :class:`ckipnlp.ws.CkipWs` output.
+        """
         return cls(*text.strip(')').rsplit('(', 1))
 
     def __str__(self):
+        return self.to_text()
+
+    def to_text(self):
+        """Transform to plain text."""
         return '{}({})'.format(self.word, self.pos)
 
     def to_dict(self):
+        """Transform to python dict/list."""
         return self._asdict() # pylint: disable=no-member
 
     def to_json(self, **kwargs):
+        """Transform to JSON format."""
         return _json.dumps(self.to_dict(), **kwargs)
 
 class WsSentence(_collections.UserList): # pylint: disable=too-many-ancestors
-    """A word-segmented sentence.
+    """A word-segmented sentence."""
 
-    Items
-        :class:`WsWord` : the words.
-    """
+    item_class = WsWord
 
     @classmethod
-    def from_text(cls, text: str):
-        """Create :class:`WsSentence` object from :class:`ckipnlp.ws.CkipWs` output."""
-        return cls(map(WsWord.from_text, text.split('\u3000')))
+    def from_text(cls, text):
+        """Create :class:`WsSentence` object from :class:`ckipnlp.ws.CkipWs` output.
+
+        Parameters
+        ----------
+            text : str
+                A sentence from :class:`ckipnlp.ws.CkipWs` output.
+        """
+        return cls(map(cls.item_class.from_text, text.split('\u3000')))
 
     def __str__(self):
+        return self.to_text()
+
+    def to_text(self):
+        """Transform to plain text."""
         return '\u3000'.join(map(str, self))
 
     def to_dict(self):
+        """Transform to python dict/list."""
         return [word.to_dict() for word in self]
 
     def to_json(self, **kwargs):
+        """Transform to JSON format."""
+        return _json.dumps(self.to_dict(), **kwargs)
+
+
+class WsSentenceList(_collections.UserList): # pylint: disable=too-many-ancestors
+    """A list of word-segmented sentence."""
+
+    item_class = WsSentence
+
+    @classmethod
+    def from_text(cls, text_list):
+        """Create :class:`WsSentenceList` object from :class:`ckipnlp.ws.CkipWs` output.
+
+        Parameters
+        ----------
+            text_list : List[str]
+                A list of sentence from :class:`ckipnlp.ws.CkipWs` output.
+        """
+        return cls(map(cls.item_class.from_text, text_list))
+
+    def __str__(self):
+        return self.to_text()
+
+    def to_text(self):
+        """Transform to plain text."""
+        return '\n'.join(map(str, self))
+
+    def to_dict(self):
+        """Transform to python dict/list."""
+        return [sentence.to_dict() for sentence in self]
+
+    def to_json(self, **kwargs):
+        """Transform to JSON format."""
         return _json.dumps(self.to_dict(), **kwargs)
