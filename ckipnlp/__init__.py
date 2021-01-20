692d64666147dbb24de4479738339bacdf1080e9
Mu Yang <emfomy@gmail.com>
2021-01-20 11:26:10 +0800

    Update version number.

--- ckipnlp/__init__.py
+++ ckipnlp/__init__.py
@@ -10,7 +10,7 @@ __author_email__ = 'emfomy@gmail.com'
 __copyright__ = '2018-2020 CKIP Lab'
 
 __title__ = 'CKIPNLP'
-__version__ = '0.10.0'
+__version__ = '1.0.0'
 __description__ = 'CKIP CoreNLP'
 __license__ = 'CC BY-NC-SA 4.0'
 
